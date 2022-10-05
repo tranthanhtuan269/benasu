d9480a5c1b2a95fd3528c7f04d41caec76717960
tuantt <tuantt@tohsoft.com>
2022-10-05 15:18:43 +0700

    first commit

---
+++ resources/views/vendor/shop/catalog/home2.blade.php
@@ -0,0 +1,19 @@
+@extends('shop::base')
+
+@section('aimeos_header')
+    <?= $aiheader['basket/mini'] ?? '' ?>
+    <?= $aiheader['catalog/tree'] ?? '' ?>
+    <?= $aiheader['catalog/home'] ?? '' ?>
+@stop
+
+@section('aimeos_head_basket')
+    <?= $aibody['basket/mini'] ?? '' ?>
+@stop
+
+@section('aimeos_head_nav')
+    <?= $aibody['catalog/tree'] ?? '' ?>
+@stop
+
+@section('aimeos_body')
+    <?= $aibody['catalog/home'] ?? '' ?>
+@stop
