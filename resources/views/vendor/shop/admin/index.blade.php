5d65cb00b51d4960de48b3fb88261ae58fbdba3b
tran thanh tuan <tran.thanh.tuan269@gmail.com>
2022-10-25 22:44:40 +0700

    css

--- resources/views/vendor/shop/admin/index.blade.php
+++ resources/views/vendor/shop/admin/index.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<title>Aimeos administration interface</title>
+		<title>Benasu administration interface</title>
 
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4/css/font-awesome.min.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4/dist/css/bootstrap.min.css" />
