49293a6089eb9d93c7e8728d49a8c64770003462
tran thanh tuan <tran.thanh.tuan269@gmail.com>
2022-10-26 20:04:23 +0700

    css

--- packages/benasu/templates/admin/jqadm/page.php
+++ packages/benasu/templates/admin/jqadm/page.php
@@ -244,6 +244,18 @@ $after = is_array( $after ) ? $after['_'] ?? reset( $after ) : $after;
 					</span>
 				</li>
 
+				<li class="treeview menuitem-language">
+					<span class="item-group">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text" viewBox="0 0 16 16">
+							<path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+							<path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+						</svg>
+						<a href="/admin/pages">
+							<span class="title">Pages</span>
+						</a>
+					</span>
+				</li>
+
 				<?php if( $this->access( $this->config( 'admin/jqadm/resource/language/groups', [] ) ) ) : ?>
 
 					<li class="treeview menuitem-language <?= $after === null ? 'after' : '' ?>">
